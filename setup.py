0e2d5f76837d4c5a8803ff5b2567e3b52554e641
Yanko <yscosta62@gmail.com>
2021-04-16 21:12:38 -0300

    Versão 1 do setuo.py

--- setup.py
+++ setup.py
@@ -1,51 +1,9 @@
 #executar pela prompt de comando do PyCharm
 #python setup.py build
 
-# from cx_Freeze import setup, Executable
-#
-# packages = ["os", "sys", "json", "PyQt5.QtWidgets", "PyQt5.QtCore", "PyQt5.QtGui",
-#              "smtplib", "ssl", "email"]
-#
-# # packages = ["os", "sys", "json", "PyQt5", "smtplib", "ssl", "email"]
-#
-# build_exe = {'packages': packages,
-#              'include_msvcr': True,
-#              'include_files': [("e-mail-icon.jpg")], }
-#
-# setup(name = "Send_CV_Out",
-#       version = "1.0.0",
-#       options = {"build_exe": build_exe},
-#       executables = [Executable("Send_CV_Out.py",
-#                                 base="Win32GUI"
-#                                 #compress=True,
-#                                 #copyDependentFiles=True,
-#                                 #appendScriptToExe=True,
-#                                 #appendScriptToLibrary=False,
-#                               )
-#                    ],
-#       )
-
-# import sys
-# from cx_Freeze import setup, Executable
-#
-# base = None
-# if sys.platform == "win32":
-#     base = "Win32GUI"
-#
-# options = {"build_exe": {"includes": "atexit"}}
-#
-# executables = [Executable("Send_CV_Out.py", base=base)]
-#
-# setup(
-#     name="Send_CV_Out",
-#     version="1.0",
-#     description="Send CV Out by e-mail from a e-mail list.",
-#     options=options,
-#     executables=executables,
-# )
-
-
 import sys
+import os
+import shutil
 from cx_Freeze import setup, Executable
 
 base = None
@@ -54,12 +12,18 @@ if sys.platform == "win32":
 
 packages = ["os", "sys", "json", "PyQt5", "smtplib", "ssl", "email"]
 
-# options = {
-#     "build_exe": {"includes": "atexit", "zip_include_packages": ["PyQt5"]}
-# }
+includes = []
+
+excludes =["sqlite3", "tensorboard", "tensorflow", "test", "tkinter", "graphviz",
+           "keras", "matplotlib", "numba", "scipy", "sqlalchemy"]
+
+include_files = ["e-mail-icon.jpg"]
 
 options = {
-    "build_exe": {"includes": "atexit", "zip_include_packages": packages}
+    "build_exe": {"includes": includes,
+                  "excludes": excludes,
+                  "zip_include_packages": packages,
+                  "include_files": include_files}
 }
 
 executables = [Executable("Send_CV_Out.py", base=base)]
@@ -71,3 +35,9 @@ setup(
     options=options,
     executables=executables,
 )
+
+# Transfere a base de dados para área do executável.
+if os.path.exists('build//exe.win-amd64-3.7'):
+    os.makedirs('build//exe.win-amd64-3.7//DB')
+    shutil.copyfile('DB//emails.csv', 'build//exe.win-amd64-3.7//DB//emails.csv')
+    shutil.copyfile('DB//e-mail-struct.json', 'build//exe.win-amd64-3.7//DB//e-mail-struct.json')
